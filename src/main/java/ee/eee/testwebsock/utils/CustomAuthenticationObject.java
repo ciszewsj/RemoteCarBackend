e5cd3fb1993516b2f913232936c80cad634e3cfb
jciszewski <jciszewski@amusys.pl>
2023-05-23 22:05:10 +0200

    remake

--- src/main/java/ee/eee/testwebsock/utils/CustomAuthenticationObject.java
+++ src/main/java/ee/eee/testwebsock/utils/CustomAuthenticationObject.java
@@ -22,7 +22,7 @@ public class CustomAuthenticationObject extends Jwt implements Principal {
 		super(tokenValue, issuedAt, expiresAt, headers, claims);
 
 		this.email = claims.get("email").toString();
-		this.id = claims.get("sid").toString();
+		this.id = claims.get("sub").toString();
 		this.name = claims.get("name").toString();
 	}
 
